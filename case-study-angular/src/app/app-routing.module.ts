abfe7599b2053040f3fa1764b968f449d3e829e0
NguyenHoang272 <nguyenhoang29082001@gmail.com>
2023-02-09 16:28:18 +0700

    ss

--- case-study-angular/src/app/app-routing.module.ts
+++ case-study-angular/src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {FacilityListComponent} from "./facility/facility-list/facility-list.component";
 import {CustomerListComponent} from "./customer/customer-list/customer-list.component";
-import {AddFacilityComponent} from "./facility/add-facility/add-facility.component";
+import {FacilityAddComponent} from "./facility/facility-add/facility-add.component";
+import {FacilityEditComponent} from "./facility/facility-edit/facility-edit.component";
+import {CustomerAddComponent} from "./customer-add/customer-add.component";
+import {CustomerEditComponent} from "./customer-edit/customer-edit.component";
+
 
 
 const routes: Routes = [
@@ -12,7 +16,10 @@ const routes: Routes = [
   {path: 'home',component:HomeComponent},
   {path: 'facility',component:FacilityListComponent},
   {path: 'customer',component:CustomerListComponent},
-  {path: 'add-facility',component:AddFacilityComponent},
+  {path: 'facility-add',component:FacilityAddComponent},
+  {path: 'facility-edit',component:FacilityEditComponent},
+  {path: 'customer-add',component:CustomerAddComponent},
+  {path: 'customer-edit',component:CustomerEditComponent},
 
 ];
 
--- case-study-angular/src/app/app.module.ts
+++ case-study-angular/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FacilityListComponent } from './facility/facility-list/facility-list.component';
-import { AddFacilityComponent } from './facility/add-facility/add-facility.component';
 import { HomeComponent } from './home/home.component';
-import {EditFacilityComponent} from "./facility/edit-facility/edit-facility.component";
 import { CustomerListComponent } from './customer/customer-list/customer-list.component';
-
+import { FacilityAddComponent } from './facility/facility-add/facility-add.component';
+import { FacilityEditComponent } from './facility/facility-edit/facility-edit.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
 
 @NgModule({
     declarations: [
@@ -20,10 +21,12 @@ import { CustomerListComponent } from './customer/customer-list/customer-list.co
         FooterComponent,
         NavigationComponent,
         FacilityListComponent,
-        AddFacilityComponent,
         HomeComponent,
-        EditFacilityComponent,
         CustomerListComponent,
+        FacilityAddComponent,
+        FacilityEditComponent,
+        CustomerAddComponent,
+        CustomerEditComponent,
     ],
   imports: [
     BrowserModule,
