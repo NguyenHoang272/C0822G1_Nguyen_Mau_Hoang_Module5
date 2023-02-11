9afb69a5e1fcc3d0a6793876e4f6f0e0c1c205e2
NguyenHoang272 <nguyenhoang29082001@gmail.com>
2023-02-11 12:42:33 +0700

    ss

--- case-study-angular/src/app/contract/contrac-list/contrac-list.component.ts
+++ case-study-angular/src/app/contract/contrac-list/contrac-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Contract} from "../../model/contract";
 
 @Component({
   selector: 'app-contrac-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contrac-list.component.css']
 })
 export class ContracListComponent implements OnInit {
+  contractList: Contract[] = [
+    {id: 1, startDate: '11/12/2002', endDate: '22/12/2002', deposit: 222222222, total: 10111111}
+  ];
 
   constructor() { }
 
